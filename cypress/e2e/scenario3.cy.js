f30dbb2c026449939bb5af0e87bd25b43f489049
Skaree86 <126167139+Skaree86@users.noreply.github.com>
2023-05-31 11:45:10 +0200

    ajout de l'html agrege

--- cypress/e2e/scenario3.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Magento', () => {
-  it('La validation du produit', () => {
-    cy.visit('https://magento.softwaretestingboard.com/breathe-easy-tank.html');
-    cy.get('.showcart').click()
-      .should('be.visible');
-    cy.contains('Proceed to checkout').click();
-  })
-})
